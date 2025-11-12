32e735d55163795d79ea62932112c9e1c5e9d757
PhzzX0 <paulopedrohof@gmail.com>
2025-11-11 23:43:54 -0300

    updating

---
+++ static/tailwind-config.js
@@ -0,0 +1,16 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            fontFamily: {
+                orbitron: ['Orbitron', 'sans-serif'],
+                rubik: ['Rubik', 'sans-serif']
+            },
+            colors: {
+                primary: '#CE272A',
+                secondary: '#781718',
+                dark: '#0A0908',
+                light: '#FBFEF9'
+            }
+        }
+    }
+}
